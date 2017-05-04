18505db3c13c7ae43c2b3442c9b537693a1762d5
Valerii Kipkaev <kipkaev@kipkaev.name>
2017-05-04 12:42:26 +0600

    only en & ru locales

--- src/SypexGeo/Reader.php
+++ src/SypexGeo/Reader.php
@@ -11,12 +11,9 @@ use SypexGeo\SxGeo;
 class Reader
 {
     protected $db = null;
-    protected $locale = null;
 
-    public function __construct($db, $locale) {
+    public function __construct($db) {
         $this->db = $db;
-        $locales = ['en', 'ru'];
-        $this->locale = ((in_array($locale, $locales)) ? $locale : 'en');
     } 
 
     public function getGeo($ip)
@@ -34,17 +31,29 @@ class Reader
             if((ip2long($ip) >= 167772160 && ip2long($ip) <= 184549375)
                 || (ip2long($ip) >= 2886729728 && ip2long($ip) <= 2887778303)
                 || (ip2long($ip) >= 3232235520 && ip2long($ip) <= 3232301055)) { //networks classes A,B,C
-                $data['country'] = 'LO';
-                $data['region'] = 'Local Network';
-                $data['city'] = 'Local Network';
+                $data['country']['iso'] = 'LO';
+                $data['country']['en'] = 'Local network';
+                $data['country']['ru'] = 'Локальная сеть';
+                $data['region']['en'] = 'Local Network';
+                $data['region']['ru'] = 'Локальная сеть';
+                $data['city']['en'] = 'Local Network';
+                $data['city']['ru'] = 'Локальная сеть';
             } elseif((ip2long($ip) >= 2130706432 && ip2long($ip) <= 2147483647)) {
-                $data['country'] = 'LO';
-                $data['region'] = 'Loopback';
-                $data['city'] = 'Loopback';
+                $data['country']['iso'] = 'LO';
+                $data['country']['en'] = 'Localhost';
+                $data['country']['ru'] = 'Локальный хост';
+                $data['region']['en'] = 'Loopback';
+                $data['region']['ru'] = 'Петля обратной связи';
+                $data['city']['en'] = 'Loopback';
+                $data['city']['ru'] = 'Петля обратной связи';
             } else {
-                $data['country'] = ($geo['country']['iso'] == null) ? 'UN' : $geo['country']['iso'];
-                $data['region'] = ($geo['region']['name_'.$this->locale] == null) ? 'Unknown' : $geo['region']['name_'.$this->locale];
-                $data['city'] = ($geo['city']['name_'.$this->locale] == null) ? 'Unknown' : $geo['city']['name_'.$this->locale];
+                $data['country']['iso'] = ($geo['country']['iso'] == null) ? 'UN' : $geo['country']['iso'];
+                $data['country']['en'] = ($geo['country']['name_en'] == null) ? 'Unknown' : $geo['country']['name_en'];
+                $data['country']['ru'] = ($geo['country']['name_ru'] == null) ? 'Неизвестно' : $geo['country']['name_ru'];
+                $data['region']['en'] = ($geo['region']['name_en'] == null) ? 'Unknown' : $geo['region']['name_en'];
+                $data['region']['ru'] = ($geo['region']['name_ru'] == null) ? 'Unknown' : $geo['region']['name_ru'];
+                $data['city']['en'] = ($geo['city']['name_en'] == null) ? 'Unknown' : $geo['city']['name_en'];
+                $data['city']['ru'] = ($geo['city']['name_ru'] == null) ? 'Unknown' : $geo['city']['name_ru'];
             }   
         } catch (\Exception $e) {
             $data = $e->getMessage();
